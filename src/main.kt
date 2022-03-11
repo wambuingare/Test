fedbffca5ba03837634f08d299a8148fb4708fde
Wambuingare <wambuinancyngare@gmail.com>
2022-03-11 09:23:42 +0300

    Done

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main(){
+    school ()
+    modulus ()
+    var p ="Hi,my name is Nancy and i am 23 years old."
+    println(p)
+    var s = z()
+
+}
+
+
+fun school (){
+    var name = "Akirachix"
+    println(name[1].toString()+name[3]+name[6])
+}
+fun modulus(){
+    var w = 25
+    var z = 6
+    var modulus = 25%6
+    println(modulus)
+}
+fun stmt(name:String,age:Int): String {
+    var p = name.toString()+ age
+    return p
+}
+fun z(){
+    var txt = "Wambui"
+    var s = txt.length
+    println(s)
+}
